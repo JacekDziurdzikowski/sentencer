bf3e14ac4be83785244f6b1dbbcf947741461151
Jacek Dziurdzikowski <jacek.dziurdzikowski@gmail.com>
2018-03-29 03:02:43 +0800

    Mechanism for parsing respose from POS api finished - now the response is parsed to assoc array

--- POSApiClient/POSApiClient.php
+++ POSApiClient/POSApiClient.php
@@ -15,8 +15,9 @@ use Unirest;
 class POSApiClient
 {
     private static $endpoint = 'https://parts-of-speech.info/tagger/postagger';
-    private static $sentence = 'Jack is very strong';
+    private static $sentence = 'Jack is very handsome';
     private static $language = 'en';
+    private static $configurationFile = __DIR__. '/posApiConfiguration.json';
 
     public static function getResponse(string $sentence)
     {
@@ -35,65 +36,64 @@ class POSApiClient
         }
     }
 
-    //TODO: function need to be rewritten in better way
-    private static function constructQuery()
+    /**
+     * This function returns useful associative array of type 'word' => 'partOfSpeech'
+     * made from raw output from parts-of-speech API
+     *
+     * @param string $response
+     * @return array
+     */
+    private static function parseResponse(string $response)
     {
-        $text = urlencode(trim(self::$sentence));
-
-        $query = self::$endpoint . "?";
-        $query .= "text=" . $text . "&";
-        $query .= "language=" . $language;
-
-        return $query;
-    }
-
-    private static function parseResponse(string $response){
         //unpack real message from the response string
         preg_match('/\{(?:[^{}]|(?R))*\}/', $response, $matches);
         $jsonResponse = $matches[0];
         $message = (json_decode($jsonResponse, true));
         $message = $message['taggedText'];
 
-        return $message;
+        //parse configuration
+        $tagMap = JsonParser::parseJson(self::$configurationFile)['tagMap'];
+
+        //parse the obtained string to useful associative array
+        $words = explode(' ', $message);
+
+
+        /**
+         * Assoc Array containing word under key and 'pos-tag' and 'pos-name' describing part of speech in value array
+         * under accordingly keys
+         * @var $wordTagArr array
+         */
+        $wordPosArr = [];
+        /**
+         * temp var helper to store last word in loop
+         * @var $lastWordTemp string
+         */
+        $lastWordTemp = "";
+
+        // loop which result is complete $wordTagArrs
+        foreach ($words as $index => $value) {
+            $tag = substr($value, strrpos($value, '_') + 1);
+            $word = substr($value, 0, strrpos($value, '_'));
+
+            $word = str_replace("\\/", "/", $word);
+            $word = str_replace("-LRB-", "(", $word);
+            $word = str_replace("-RRB-", ")", $word);
+
+            if ($tag != '$,' && $tag != '$.' && $lastWordTemp != '``' && $word != '\'\'' && $word != ')' && $lastWordTemp != '(') {
+                //$taggedHTML += ' ';
+            }
+
+            $lastWordTemp = $word;
+            $word = str_replace('``', '"', $word);
+            $word = str_replace('\'\'', '"', $word);
+            if ($word == '"') {
+                $tag = '';
+            }
+
+            $wordPosArr[$word]['pos-tag'] = $tag ?: '';
+            $wordPosArr[$word]['pos-name'] = $tagMap[$tag][0] ?: '';
+        }
+
+        return $wordPosArr;
     }
 }
-
-/*function callback(data) {
-
-    var tagMap = appData.tagMap;
-    var color = appData.colors;
-
-    $('#form').removeClass('mode-edit').addClass('mode-view');
-    $('#tagTipContainer').hide();
-
-    var words = data.taggedText.split(" ");
-    var taggedHTML = "";
-    var lastWord = "";
-    $.each(words, function(index, taggedWord) {
-        var tag = taggedWord.substring(taggedWord.lastIndexOf("_") + 1);
-        var word = taggedWord.substring(0, taggedWord.lastIndexOf("_"));
-        word = word.replace("\\/", "/");
-        word = word.replace("-LRB-", "(");
-        word = word.replace("-RRB-", ")");
-        // TODO: are there other symbols?
-        if (tag != '$,' && tag != '$.' && lastWord != '``' && word != '\'\'' && word != ')' && lastWord != '(') {
-            taggedHTML += ' ';
-        }
-        lastWord = word;
-        word = word.replace('``', '"');
-        word = word.replace('\'\'', '"');
-        if (word == '"') {
-            tag = '';
-        }
-        // TODO: escape html
-        if (tagMap[tag] != undefined && color[tagMap[tag][0]] != undefined) {
-            taggedHTML += '<span class="taggedWord" style="background-color: ' + color[tagMap[tag][0]] + '">' + word + '<span>' + tag + '</span></span>';
-        }
-        else {
-            taggedHTML += '<span class="taggedWord">' + word + '<span>' + tag + '</span></span>';
-        }
-    });
-    $('#textTagged').html(taggedHTML);
-
-
-    / \{(?:[^{}]|(?R))*\}/x*/
